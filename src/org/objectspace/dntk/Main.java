85e5c68ac0f65ccce4462e8049c7f112e826ac50
Jürgen Enge <je4@users.noreply.github.com>
2017-08-02 11:19:08 +0200

    initial test

--- src/org/objectspace/dntk/Main.java
+++ src/org/objectspace/dntk/Main.java
@@ -3,6 +3,14 @@
  */
 package org.objectspace.dntk;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.glassfish.jersey.servlet.ServletContainer;
+
+
 /**
  * @author juergen.enge
  *
@@ -13,8 +21,27 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        Server server = new Server(8080);
 
-	}
+        ServletContextHandler ctx = 
+                new ServletContextHandler(ServletContextHandler.NO_SESSIONS);
+                
+        ctx.setContextPath("/");
+        server.setHandler(ctx);
 
+        ServletHolder serHol = ctx.addServlet("ServletContainer.class", "/rest/*");
+        serHol.setInitOrder(1);
+        serHol.setInitParameter("jersey.config.server.provider.packages", 
+                "com.zetcode.res");
+
+        try {
+            server.start();
+            server.join();
+        } catch (Exception ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+
+            server.destroy();
+        }
+    }
 } 
