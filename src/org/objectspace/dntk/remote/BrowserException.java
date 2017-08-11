177d35d5030d26f5e4ef11792cd2d6d6a1f92804
Jürgen Enge <je4@users.noreply.github.com>
2017-08-11 17:54:56 +0200

    commercial license added

--- src/org/objectspace/dntk/remote/BrowserException.java
+++ src/org/objectspace/dntk/remote/BrowserException.java
@@ -7,6 +7,8 @@
  *     it under the terms of the GNU General Public License as published by
  *     the Free Software Foundation, either version 3 of the License, or
  *     (at your option) any later version.
+ *     
+ *     If you need a commercial license please contact info-age GmbH, Basel.
  *
  *     DNTK - Digital Narration ToolKit is distributed in the hope that it will be useful,
  *     but WITHOUT ANY WARRANTY; without even the implied warranty of
